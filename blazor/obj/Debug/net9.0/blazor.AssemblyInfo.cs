f0c25b7dd3f2d0071ea5ac2b5adc4b9df69925ae
Van Der Merwe <Wouter.VanDerMerwe@SamancorCr.com>
2025-10-10 13:29:35 +0200

    Fine tuning

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465596f9562fffcf4d152bdfe46af5b9ddf7898b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5d754b51cb129349fdf0bb38e5853d5929b8da")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
