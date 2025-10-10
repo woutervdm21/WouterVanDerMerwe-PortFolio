6c5d754b51cb129349fdf0bb38e5853d5929b8da
Van Der Merwe <Wouter.VanDerMerwe@SamancorCr.com>
2025-10-10 10:12:05 +0200

    Finished Hero Section with background animations (Particle.js), microsoft logo & quick links

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b887ecaa1391cf36d2760f54d1bd96fd14a8603d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465596f9562fffcf4d152bdfe46af5b9ddf7898b")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ script.js
@@ -0,0 +1,52 @@
+    // Tailwind dark mode config
+    tailwind.config = {
+      darkMode: 'class',
+      theme: {
+        extend: {}
+      }
+    }
+
+    
+    // Theme switcher initialization
+    document.addEventListener('DOMContentLoaded', () => {
+      const savedTheme = localStorage.getItem('theme');
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      
+      if (savedTheme === 'light') {
+        document.documentElement.classList.remove('dark');
+      } else if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      } else if (prefersDark) {
+        document.documentElement.classList.add('dark');
+      }
+      
+      updateThemeIcon();
+    });
+    
+    function toggleTheme() {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
+      
+      updateThemeIcon();
+    }
+    
+    function updateThemeIcon() {
+      const isDark = document.documentElement.classList.contains('dark');
+      const moonIcon = document.getElementById('moon-icon');
+      const sunIcon = document.getElementById('sun-icon');
+      
+      if (isDark) {
+        moonIcon.classList.add('hidden');
+        sunIcon.classList.remove('hidden');
+      } else {
+        moonIcon.classList.remove('hidden');
+        sunIcon.classList.add('hidden');
+      }
+    }
+
+    
