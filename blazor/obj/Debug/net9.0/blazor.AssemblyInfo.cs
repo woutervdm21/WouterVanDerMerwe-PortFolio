b54c4ca08fb0ca1a06ad301f8b9ee8f2179f254a
unknown <woutervdm21@gmail.com>
2025-10-11 09:01:07 +0200

    BU

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f89dacf7d668410a06f496abcbb1958173b40d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e04789dfb379b84f0b139c6734bfaa038b8dca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
